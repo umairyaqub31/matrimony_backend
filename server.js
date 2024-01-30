2b6a327334e8e19a4f9868470383688a32be6c3b
berryboostbb <berryboostbb@gmail.com>
2024-01-30 11:03:39 +0500

    socket added

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
 app.use(cors());
 const dbConnect = require("./database/index");
 const ErrorHandler = require("./middlewares/errorHandler");
@@ -14,7 +17,32 @@ app.use(userRouter);
 app.use(adminRouter);
 
 dbConnect();
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5001",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log("data....", data);
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 app.use(ErrorHandler);
 app.listen(PORT, () => {
-  console.log("server running")
+  console.log("server running");
 });
