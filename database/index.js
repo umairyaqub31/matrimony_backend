27a7c2699fb3df129b8ad55fb45173fa63d750be
berryboostbb <berryboostbb@gmail.com>
2024-02-06 16:01:58 +0500

    minor change

--- database/index.js
+++ database/index.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const dbConnect = async () => {
-    try {
-        mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
-        console.log(`Database connected to host: ${conn.connection.host}`);
-        
-    } catch (error) {
-        console.log(`Error: ${error}`);
-    }
-}
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Database connected to host: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
 
 module.exports = dbConnect;
